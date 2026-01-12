8d4695a39c49e06e58232e0a576eaa40b670fa1b
hhoang1204 <hhoang12042003@gmail.com>
2026-01-12 11:09:50 +0700

    setValue and patchValue

--- s18-template-driven-form/src/app/app.component.ts
+++ s18-template-driven-form/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   firstName = '';
   lastName = '';
   emailAddress = '';
+  dateOfBirth = '';
 
   @ViewChild('registrationForm') registrationForm: NgForm | undefined;
 
@@ -27,5 +28,51 @@ export class AppComponent {
     console.log(this.registrationForm?.controls['firstName'].value);
     console.log(this.registrationForm?.value.lastName);
     console.log(this.registrationForm?.value.email);
+  }
+
+  generateUsername() {
+    let username = '';
+    if (this.firstName.length >= 3) {
+      username += this.firstName.slice(0, 3);
+    } else {
+      username += this.firstName;
+    }
+
+    if (this.lastName.length >= 3) {
+      username += this.lastName.slice(0, 3);
+    } else {
+      username += this.lastName;
+    }
+
+    let datetime = new Date(this.dateOfBirth);
+    username += datetime.getFullYear();
+
+    username = username.toLowerCase();
+
+    console.log(username);
+
+    // this.registrationForm?.setValue({
+    //   firstName: this.registrationForm?.value.firstName,
+    //   lastName: this.registrationForm?.value.lastName,
+    //   email: this.registrationForm?.value.email,
+    //   dob: this.registrationForm?.value.dob,
+    //   phoneNumber: this.registrationForm?.value.phoneNumber,
+    //   gender: this.registrationForm?.value.gender,
+    //   address: {
+    //     streetAddress: this.registrationForm?.value.address.streetAddress,
+    //     streetAddressLine2:
+    //       this.registrationForm?.value.address.streetAddressLine2,
+    //     country: this.registrationForm?.value.address.country,
+    //     city: this.registrationForm?.value.address.city,
+    //     region: this.registrationForm?.value.address.region,
+    //     postalCode: this.registrationForm?.value.address.postalCode,
+    //   },
+    //   username: username,
+    // });
+
+    // patchValue: is used to update only a subset of the element of FormGroup or FormArray value
+    this.registrationForm?.form.patchValue({
+      username: username,
+    });
   }
 }
