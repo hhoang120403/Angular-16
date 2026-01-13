b612302d5a3f5d595b1fce32e4428ab823427715
hhoang1204 <hhoang12042003@gmail.com>
2026-01-13 15:52:59 +0700

    Http Get

--- s20-http/src/app/dashboard/dashboard.component.ts
+++ s20-http/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Task } from '../model/Task';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   showCreateTaskForm: boolean = false;
   http: HttpClient = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.fetchAllTaks();
+  }
 
   OpenCreateTaskForm() {
     this.showCreateTaskForm = true;
@@ -30,6 +35,28 @@ export class DashboardComponent {
         {
           headers,
         }
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
+  private fetchAllTaks() {
+    this.http
+      .get<{ [key: string]: Task }>(
+        'https://angularhttpclient-1df7a-default-rtdb.firebaseio.com/tasks.json'
+      )
+      .pipe(
+        map((res) => {
+          // Transform Data
+          let tasks = [];
+          for (let key in res) {
+            if (res.hasOwnProperty(key)) {
+              tasks.push({ ...res[key], id: key });
+            }
+          }
+          return tasks;
+        })
       )
       .subscribe((res) => {
         console.log(res);
